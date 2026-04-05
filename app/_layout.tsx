497d7d638932c72058ad65af72f5c69dc09f37ba
Ruben do Rosário <rubenmrosario87@gmail.com>
2026-04-05 23:20:38 +0100

    feat: wrap Stack component in SafeAreaView for improved layout handling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,11 @@
 import "../global.css";
 import { Stack } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}/>
+    <SafeAreaView className="flex-1">
+      <Stack screenOptions={{ headerShown: false }}/>
+    </SafeAreaView>
   )
 }
